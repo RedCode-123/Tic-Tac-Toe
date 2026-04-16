05d9443b77250f170a33326f20ea15ec31ee1d47
RedCode-123 <erar123@gmail.com>
2026-04-15 20:19:15 -0600

    add Version 1

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 "use strict";
 
 // Model
-function createPlayer(name, symbol) {
+function createPlayer(name, symbol, color, id) {
     let moves = [];
-    let lastMove = [0, 0]
+    let lastMove = [0, 0];
+    let score = 0;
+
+    function resetMoves() {
+        moves = [];
+        lastMove = [];
+    }
 
     function setLastMove(coor) {
-        // TODO comprobar que coor sea un array, y que tenga un tamaño de dos
         lastMove = coor;
         // console.error(coor[0], coor[1]);
         moves.push(coor);
@@ -30,40 +35,48 @@ function createPlayer(name, symbol) {
         let rows = getRows();
         let columns = getColumns();
         let winRowOrColumn = ["000", "111", "222"];
-        let winDiagonal = ["012", "210"];
+        let winDiagonal = ["012", "210", "102", "120", "021", "201"];
 
         if (rows.length >= 3 && columns.length >= 3) {
             let lastThreerows = rows.slice(-3);
             let lastThreeColumns = columns.slice(-3);
-            if (winRowOrColumn.includes(rows) || winRowOrColumn.includes(columns)) {
+            if (winRowOrColumn.includes(lastThreerows) || winRowOrColumn.includes(lastThreeColumns)) {
                 return true;
             }
-            if (rows === columns) {
-                if (winDiagonal.includes(rows) && winDiagonal.includes(rows)) {
+            // if (lastThreerows === lastThreeColumns) {
+                if (winDiagonal.includes(lastThreerows) && winDiagonal.includes(lastThreeColumns)) {
                    return true;
                 }
 
-            }
+            // }
         }
 
         return false;
     }
 
     function getLastCellInfo() {
-        return {symbol, "player": name, "coor": getLastMove()}
+        // Return an obj that goes into the cell
+        return {"symbol" : symbol, "player": name, "coor": getLastMove(), color}
     }
 
-    return {name, symbol,setLastMove, getLastMove , getMoves, getRows, getColumns, doIWin, getLastCellInfo};
+    return {name, symbol,setLastMove, getLastMove , getMoves, getRows, getColumns, doIWin, getLastCellInfo, color, id, score, resetMoves};
 }
 
 function createBoard() {
-        let board = [ // row, column
+    let board = [ // {"symbol", "player", "coor"}
             ["", "", ""],
             ["", "", ""],
             ["", "", ""]
         ];
 
 
+    function resetMoves() {
+        board = [ // {"symbol", "player", "coor"}
+                ["", "", ""],
+                ["", "", ""],
+                ["", "", ""]
+            ]
+    }
 
     function addMove(cellInfo) {
         // cellinfo es un diccionario con la siguiente estructura:
@@ -76,7 +89,7 @@ function createBoard() {
     }
 
     function isCellAvailable(coor) {
-        // coor = [0,0]
+        // Check if de cell (coor) is available
         if(board[coor[0]][coor[1]] === "") {
             return true;
         } else {
@@ -84,65 +97,179 @@ function createBoard() {
         }
     }
 
+    function cellsAvailable() {
+        // Return true if there is any cell available
+        let availableCells = board.flat().filter(cell => cell === "");
+        return availableCells.length === 0 ? false : true;
+    }
+
+    function getSymbol(coor) {
+        let theSymbol = board[coor[0]][coor[1]]?.symbol;
+        return theSymbol ?? "";
+    }
+
+    function getColor(coor) {
+        let theColor = board[coor[0]][coor[1]]?.color;
+        return theColor ?? "black";
+    }
     // TODO Agregar una función que compruebe si una coordenada ya estpa ocupada
 
-    return {board, addMove, isCellAvailable};
+    return {board, addMove, isCellAvailable, cellsAvailable, getSymbol, getColor, resetMoves};
 }
 
 // View
-function createView() {
-    // TODO Crear tablero
+function createView(player) {
     let boardOutput = document.querySelector(".main > .board") ;
 
-    function createBoard() {
+    function updateBoard() {
+        boardOutput.innerHTML = "";
         let coor = [0,[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]];
         for (let x= 1; x<=9; x++) {
             let cell = document.createElement("div");
             cell.classList.add("cell", `cell-${x}`);
             cell.dataset.coor = coor[x];
+            cell.textContent = board?.getSymbol(coor[x]);
+            cell.setAttribute("style", `color: ${board?.getColor(coor[x])};`)
+            cell.addEventListener("click", () => {
+                if (start) {
+                    updateState(coor[x])
+                }
+            });
+            cell.addEventListener("mouseenter", () => {
+
+                if (board.getSymbol(coor[x]) === "" ) {
+                    cell.setAttribute("style", `color: white; background-color: ${currentPlayer?.color};`)
+                   cell.textContent = currentPlayer?.symbol
+                } else {
+                    cell.setAttribute("style", `color: ${board?.getColor(coor[x])};`)
+                    cell.textContent = board?.getSymbol(coor[x])
+                }
+
+
+            });
+            cell.addEventListener("mouseleave", () => {
+                cell.setAttribute("style", `color: ${board?.getColor(coor[x])};`)
+                cell.textContent = board?.getSymbol(coor[x]);
+            });
             // cell.style.width = "33px";
             // cell.style.height = "33px";
             boardOutput.appendChild(cell);
         }
 
     }
-    return {createBoard}
+
+    function addFunctionToStart(func) {
+        const startbtn = document.querySelector("button.start");
+        startbtn.addEventListener("click",() => {
+            if (start === false) {
+                startbtn.textContent = "Stop!"
+                start = true;
+            } else {
+                startbtn.textContent = "Start!"
+                reset();
+                return;
+            }
+            func();
+            // startbtn.setAttribute("disabled", "disabled")
+            // startbtn.setAttribute("style", "display:none;")
+        });
+    }
+
+    return {updateBoard, addFunctionToStart}
 }
 
 
 // Control
-const board =  createBoard();
-const player1 = createPlayer("Eder", "O");
-const player2 = createPlayer("Raúl", "X");
 
+function reset() {
+    start = false;
+    board =  createBoard();
+    player1Name.removeAttribute("disabled");
+    player1Name.value = "";
+    player1Symbol.textContent = player2Symbol.textContent = "__";
+    player1Symbol.setAttribute("style", `font-size: 1rem; color: black;`);
+    player1.score = 0;
+    player1 = null;
+    player2Name.removeAttribute("disabled");
+    player2Name.value = "";
+    player2Symbol.setAttribute("style", `font-size: 1rem; color: black;`);
+    player2.score = 0;
+    player2 = null;
+    view =  createView();
+    view.updateBoard();
+}
+
+let board =  createBoard();
+let player1Name;
+let player2Name;
+let player1Symbol = document.querySelector(".player1symbol");
+let player2Symbol = document.querySelector(".player2symbol");
+let player1 = null;
+let player2 = null
+let currentPlayer;
 let view =  createView();
-view.createBoard();
-//console.log(game.board, game.player1.moves, game.player2.moves);
-
-console.log("Is available ", board.isCellAvailable([0,1]))
-player2.setLastMove([0,1]);
-board.addMove(player2.getLastCellInfo());
-console.log("Is available ", board.isCellAvailable([0,1]))
+view.updateBoard();
+let start = false;
+view.addFunctionToStart(updateState);
 
 
-player2.setLastMove([2,1]);
-board.addMove(player2.getLastCellInfo());
+function updateState(coor = null) {
+    if (coor === null) {
+        if (player1 === null) {
+            player1Name = document.querySelector(".player1name");
+            if (player1Name.value === "") {
+                player1Name.value = "Player 1"
+                player1 = createPlayer(player1Name.value, "O", "blue", 1)
+            } else {
+                player1 = createPlayer(player1Name.value, "O", "blue", 1)
+            }
+            player1Symbol.textContent = player1.symbol;
+            player1Symbol.setAttribute("style", `font-size: 1.5rem; color: ${player1.color};`);
+            player1Name.setAttribute("disabled", "disabled");
+        }
+        if (player2 === null) {
+            player2Name = document.querySelector(".player2name");
+            if (player2Name.value === "") {
+                player2Name.value = "Player 2"
+                player2 = createPlayer(player2Name.value, "X", "red", 2)
+            } else {
+                player2 = createPlayer(player2Name.value, "X", "red", 2)
+            }
+            player2Symbol.textContent = player2.symbol;
+            player2Symbol.setAttribute("style", `font-size: 1.5rem; color: ${player2.color};`);
+            player2Name.setAttribute("disabled", "disabled");
+        }
+            currentPlayer = player1;
+    }
+    //console.log(game.board, game.player1.moves, game.player2.moves);
 
-player1.setLastMove([2,2]);
-board.addMove(player1.getLastCellInfo());
+    if (coor !== null) {
+        if (board.isCellAvailable(coor)) {
+            currentPlayer.setLastMove(coor);
+            board.addMove(currentPlayer.getLastCellInfo());
+            if (currentPlayer.doIWin() === true) {
+                // TODO
+                let score = document.querySelector(`.player${currentPlayer.id}score`);
+                if (currentPlayer.id === 1) {
+                    player1.score = 1 + +player1.score
+                    score.textContent = player1.score;
+                    currentPlayer = player1;
+                } else {
+                    player2.score = 1 + +player2.score
+                    score.textContent = player2.score;
+                    currentPlayer = player2;
+                }
+                player1.resetMoves();
+                player2.resetMoves();
+                board.resetMoves();
+                view.updateBoard();
+                return;
+            }
+            currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
+            view.updateBoard();
+        }
+    }
 
-player1.setLastMove([1,1]);
-board.addMove(player1.getLastCellInfo());
 
-
-player1.setLastMove([0,0]);
-board.addMove(player1.getLastCellInfo());
-
-console.log(board.board);
-console.table(player1.getRows(), player1.getColumns());
-console.error("Player1 win?",player1.doIWin());
-
-console.table(player2.getRows(), player2.getColumns());
-console.error("Player2 win?",player2.doIWin());
-
+}
 
